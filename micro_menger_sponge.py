a03efa74b8c9e83ee2977252201e8002f65b1e9a
brian-hodges <brjhodges@gmail.com>
2017-01-19 17:21:38 -0600

    Simplified micro menger use of range()

--- micro_menger_sponge.py
+++ micro_menger_sponge.py
@@ -1,3 +1,3 @@
-s,q=[["██"]],range
-for i in q(int(input())):s=[["  " if x==y==1 else c for c in r for y in q(3)] for r in s for x in q(3)]
+s,q=[["██"]],[0,1,2]
+for i in range(int(input())):s=[["  " if x==y==1 else c for c in r for y in q] for r in s for x in q]
 print("\n".join(map("".join, s)))
