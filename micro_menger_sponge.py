68332526239f3ea1f28e1d73459271bc92dd1831
brian-hodges <brjhodges@gmail.com>
2017-01-19 10:40:44 -0600

    Shortened micro menger by using map instead of generator to print

--- micro_menger_sponge.py
+++ micro_menger_sponge.py
@@ -1,3 +1,3 @@
 s,q=[["██"]],range
 for i in q(int(input())):s=[["  " if x==y==1 else c for c in r for y in q(3)] for r in s for x in q(3)]
-print("\n".join("".join(r) for r in s))
+print("\n".join(map("".join, s)))
